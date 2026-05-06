b36cf64e0c8b8653fd214a4cdfbef5f35dac1e2d
Hoang Anh <anhtenlaanh00@gmail.com>
2026-05-06 22:39:58 +0700

    user class

---
+++ src/res/User.java
@@ -0,0 +1,20 @@
+package res;
+
+public abstract class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean verifyPassword(String password) {
+        return this.password.equals(password);
+    }
+
+}
