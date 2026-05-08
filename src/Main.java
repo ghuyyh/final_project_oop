d8744aaf752b475aeb4a8d0c06f923fc19405d47
ghuyyh <95045672+ghuyyh@users.noreply.github.com>
2026-05-09 02:27:48 +0700

    update class Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
-
 public class Main {
 public static void main(String[] args) {
     new GUI_MainFrame();
