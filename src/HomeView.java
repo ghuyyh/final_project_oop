ff3ed6b240a25d322ce5e147132cf9a184d71fa8
Hoang Anh <anhtenlaanh00@gmail.com>
2026-05-09 20:59:13 +0700

    khách tới chơi

--- src/HomeView.java
+++ src/HomeView.java
@@ -6,11 +6,12 @@ public class HomeView {
     private Core core = Core.getInstance();
 
     public HomeView() {
+        System.out.println("So luong san pham hot: " + core.getHotProducts().size());
 
         homePanel.setLayout(new BorderLayout());
         homePanel.setBorder(BorderFactory.createTitledBorder("Today Hot Sales"));
         JPanel hotProductsPanel = new JPanel();
-        // hotProductsPanel.setLayout(new BoxLayout(hotProductsPanel, BoxLayout.Y_AXIS));
+        hotProductsPanel.setLayout(new GridLayout(0,1,5,5));
 
         //scroll pane for hot products
         JScrollPane scrollPane = new JScrollPane(hotProductsPanel);
@@ -18,19 +19,29 @@ public class HomeView {
             JPanel productPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
             productPanel.add(new JLabel(product.getName() + " - $" + String.format("%.2f", product.getPrice())));
             JButton addToCartBtn = new JButton("Add to Cart");
-            addToCartBtn.addActionListener(e -> {
-                core.getGuestCart().addItem(product, 1);
-                JOptionPane.showMessageDialog(null, "Added " + product.getName() + " to cart!");
+
+            addToCartBtn.addActionListener(e -> { 
+                User loggedInUser = core.getLoggedInUser();
+                if(loggedInUser instanceof Admin){
+                    JOptionPane.showMessageDialog(null, "Admins cannot add products to cart. Please log in as a customer.");
+                }
+                else {
+                    Cart targetCart;
+                    if (loggedInUser instanceof Customer) {
+                        targetCart = ((Customer) loggedInUser).getPersonalCart();
+                    } else {
+                        targetCart = core.getGuestCart();
+                    }
+                    targetCart.addItem(product, 1);
+                    JOptionPane.showMessageDialog(null," Added" + product.getName() + " to cart!");
+                }
             });
             productPanel.add(addToCartBtn);
             hotProductsPanel.add(productPanel);
         }
         homePanel.add(scrollPane, BorderLayout.CENTER);
-
     }
-
     public JPanel getHomePanel() {
         return homePanel;
     }
-
 }
