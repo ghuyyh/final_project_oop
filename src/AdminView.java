0c769f336e083f540a3cdb6aee728c9367b63030
Hoang Anh <anhtenlaanh00@gmail.com>
2026-05-09 23:12:23 +0700

    private admin

--- src/AdminView.java
+++ src/AdminView.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 public class AdminView {
     private JPanel adminPanel = new JPanel();
-   
+   private JTextField idField = new JTextField(15);
+    private JTextField nameField = new JTextField(15);
+    private JTextField priceField = new JTextField(15);
+    private JTextField stockField = new JTextField(15);
 
     public AdminView() {
         adminPanel.setLayout(new BorderLayout());
