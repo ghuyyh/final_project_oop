241dbd5a2168c13ca7b6c10fde7d548cdca2c5bc
ghuyyh <95045672+ghuyyh@users.noreply.github.com>
2026-05-09 21:48:42 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
-import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            System.out.println("Failed to set system look and feel");
-        }
+       
         new GUI_MainFrame();
     }
 }
