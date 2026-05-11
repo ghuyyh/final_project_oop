12dc318dfe8f24cd6dd105d8bf338b637e54f88b
Hoang Anh <anhtenlaanh00@gmail.com>
2026-05-11 22:52:03 +0700

    core update

--- src/Core.java
+++ src/Core.java
@@ -21,11 +21,24 @@ public class Core {
         usersDatabase.add(new Admin("admin", "admin123"));
         usersDatabase.add(new Customer("customer", "customer123"));
 
-        inventory.add(new Product("P01", "Xiaomi 13", 1400.0, 10, "xiaomi13.png"));
-        inventory.add(new Product("P02", "iPhone 15 Pro Max", 1000.0, 5, "iphone15promax.png"));
-        inventory.add(new Product("P03", "Samsung Galaxy S23 Ultra", 800.0, 8, "samsunggalaxys23ultra.png"));
-        inventory.add(new Product("P04", "OnePlus 11", 230.0, 12, "oneplus11.png"));
-        inventory.add(new Product("P05", "Google Pixel 8 Pro", 888.0, 7, "googlepixel8pro.png"));
+        inventory.add(new Phone("P01", "Xiaomi 13", 1400.0, 10, "xiaomi13.png", "Xiaomi"));
+        inventory.add(new Phone("P02", "iPhone 15 Pro Max", 1000.0, 5, "iphone15promax.png", "Apple"));
+        inventory.add(new Phone("P03", "Samsung Galaxy S23 Ultra", 800.0, 8, "samsunggalaxys23ultra.png", "Samsung"));
+        inventory.add(new Phone("P04", "OnePlus 11", 230.0, 12, "oneplus11.png", "OnePlus"));
+        inventory.add(new Phone("P05", "Google Pixel 8 Pro", 888.0, 7, "googlepixel8pro.png", "Google"));
+
+        inventory.add(new SmartHome("S01", "Google Nest Hub", 100.0, 15, "googlenesthub.png", "Google"));
+        inventory.add(new SmartHome("S02", "Amazon Echo Show 10", 150.0, 10, "amazonechoshow10.png", "Amazon"));
+        inventory.add(new SmartHome("S03", "Apple HomePod Mini", 99.0, 20, "applehomepodmini.png", "Apple"));
+        inventory.add(new SmartHome("S04", "Samsung SmartThings Hub", 120.0, 5, "samsungsmartthingshub.png", "Samsung"));
+        inventory.add(new SmartHome("S05", "Xiaomi Mi Smart Speaker", 80.0, 18, "xiaomismartspeaker.png", "Xiaomi"));
+
+        inventory.add(new Other( "O01", "Wireless Charger", 25.0, 30, "wirelesscharger.png", "Charger"));
+        inventory.add(new Other( "O02", "Phone Case", 15.0, 50, "phonecase.png", "Accessory"));
+        inventory.add(new Other( "O03", "Screen Protector", 10.0, 40, "screenprotector.png", "Accessory"));
+        inventory.add(new Other( "O04", "Bluetooth Earbuds", 50.0, 25, "bluetoothearbuds.png", "Audio"));
+        inventory.add(new Other( "O05", "Power Bank", 40.0, 20, "powerbank.png", "Charger"));
+        
         hotProducts.addAll(inventory);
 
     }
