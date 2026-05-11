8ded0542c508209635a6d52b4282f70ddf5ac20a
Hoang Anh <anhtenlaanh00@gmail.com>
2026-05-11 11:42:53 +0700

    add image in product

--- src/Product.java
+++ src/Product.java
@@ -3,10 +3,13 @@ public class Product {
     private String name;
     private double price;
     private int stockQuantity;
-    public Product(String id, String name, double price, int stockQuantity) {
+    private String imageFileName;
+
+    public Product(String id, String name, double price, int stockQuantity, String imageFileName) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.imageFileName = imageFileName;
         this.stockQuantity = stockQuantity;
     }
     public boolean reduceStock (int amount){
@@ -20,13 +23,16 @@ public class Product {
         return this.price;
     }
     public String getDetails() {
-        return "ID: " + id + ", Name: " + name + ", Price: $" + price + ", Stock: " + stockQuantity;
+        return "ID: " + id + ", Name: " + name + ", Price: $" + price + ", Stock: " + stockQuantity + ", Image: " + imageFileName;
     }
     public String getId() {
         return id;
     }
     public String getName() {
         return name;
+    }
+    public String getImageFileName() {
+        return imageFileName;
     }
     public void setPrice(double price) {
         this.price = price;
